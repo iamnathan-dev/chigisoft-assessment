8db959d1015178c591f6a505f0d86b28cc59037e
iamnathan-dev <nathancodes05@gmail.com>
2025-05-22 22:59:25 +0100

    wip: responsive navbar ui

--- src/shared/components/navbar.tsx
+++ src/shared/components/navbar.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Dialog, DialogPanel, PopoverGroup } from "@headlessui/react";
+import { PopoverGroup } from "@headlessui/react";
 import Image from "next/image";
-import { CircleX, Heart, Menu, Search, ShoppingCart, User } from "lucide-react";
+import { Heart, Menu, Search, ShoppingCart, User } from "lucide-react";
 import Logo from "@/shared/assets/images/logo.png";
 import LogoMobile from "@/shared/assets/images/logo-mb.png";
 import { Input } from "@/components/ui/input";
@@ -24,6 +24,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 interface PriceOption {
   value: string;
@@ -51,7 +52,6 @@ const iconsBtns: IconButton[] = [
 ];
 
 export default function Navbar() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
@@ -81,15 +81,77 @@ export default function Navbar() {
             />{" "}
           </Link>
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+        <div className="flex gap-5 lg:hidden">
+          <Button
+            variant={"secondary"}
+            size={"icon"}
+            className="text-gray-700 rounded-full relative shadow-none focus:ring-0 focus-visible:ring-0 cursor-pointer"
+            onClick={() => {}}
           >
             <span className="sr-only">Open main menu</span>
             <Menu strokeWidth={1} />
-          </button>
+          </Button>
+          {[
+            { icon: Search, showBadge: false, tooltip: "Search" },
+            { icon: User, showBadge: false, tooltip: "Account" },
+          ].map((item, index) =>
+            item.icon === Search ? (
+              <Dialog key={index}>
+                <DialogTrigger asChild>
+                  <Button
+                    variant={"secondary"}
+                    size={"icon"}
+                    className="text-gray-700 rounded-full relative shadow-none focus:ring-0 focus-visible:ring-0 cursor-pointer"
+                  >
+                    <Search strokeWidth={1} />
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px] p-2 top-15 translate-y-0">
+                  <div className="flex flex-row w-full border rounded-sm">
+                    <div className="flex flex-row items-center gap-2 py-1 px-2">
+                      <Search strokeWidth={1} />
+                      <Input
+                        type="text"
+                        placeholder="Search for products"
+                        className="w-full shadow-none border-0 focus:ring-0 focus-visible:ring-0 p-0"
+                      />
+                    </div>
+                    <div className="border-l flex flex-row items-center">
+                      <Select>
+                        <SelectTrigger className="border-0 rounded-none cursor-pointer shadow-none focus:ring-0 focus-visible:ring-0">
+                          <SelectValue placeholder="Price" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectGroup>
+                            <SelectLabel>Prices</SelectLabel>
+                            {priceOPtions.map(({ value, label }) => (
+                              <SelectItem key={value} value={value}>
+                                {label}
+                              </SelectItem>
+                            ))}{" "}
+                          </SelectGroup>
+                        </SelectContent>
+                      </Select>
+                    </div>
+                  </div>
+                </DialogContent>
+              </Dialog>
+            ) : (
+              <Button
+                key={index}
+                variant={"secondary"}
+                size={"icon"}
+                className="text-gray-700 rounded-full relative shadow-none focus:ring-0 focus-visible:ring-0 cursor-pointer"
+              >
+                {item.showBadge && (
+                  <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
+                    0
+                  </span>
+                )}
+                <item.icon strokeWidth={1} />
+              </Button>
+            )
+          )}
         </div>
         <PopoverGroup className="hidden lg:block w-[800px]">
           <div className="flex flex-row w-full border rounded-sm">
@@ -148,65 +210,6 @@ export default function Navbar() {
           </div>
         </div>
       </nav>
-      <Dialog
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-        className="lg:hidden"
-      >
-        <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Cartzilla</span>
-              <Image
-                alt="cartzilla logo mobile"
-                src={LogoMobile}
-                className="h-8 w-auto"
-              />
-            </a>
-            <button
-              type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            >
-              <span className="sr-only">Close menu</span>
-              <CircleX strokeWidth={1} />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Features
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Marketplace
-                </a>
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Company
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </DialogPanel>
-      </Dialog>
     </header>
   );
 }
